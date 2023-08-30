2391d82bdf2ac5e8b4fcded7c0454f04bd8a2b56
Caique Moraes <ike.moraes93@gmail.com>
2023-08-30 09:59:03 -0300

    feature: GetUserProfile

---
+++ src/application/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,49 @@
+import { provide } from '../registry'
+import { InMemoryUsersRepository } from 'tests/repositories/in-memory-users-repository'
+import { SuccessResponse } from '@/infra/http/entities/success-response'
+import { User } from '../entities/user'
+import { PasswordHash } from '@/core/entities/password-hash'
+import { GetUserProfileUseCase } from './get-user-profile'
+
+describe('GetUserProfile use case', () => {
+  let usersRepository: InMemoryUsersRepository
+  let sut: GetUserProfileUseCase
+
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    provide('usersRepository', usersRepository)
+    sut = new GetUserProfileUseCase()
+  })
+
+  it('should be able to get user by id', async () => {
+    const passwordHash = new PasswordHash()
+    const name = 'John Doe'
+    const email = 'johm@doe.com'
+    const password = '123456'
+
+    const { id } = await usersRepository.create({
+      name,
+      email,
+      password_hash: await passwordHash.createHash(password),
+    })
+
+    const result = await sut.execute({
+      userId: id.toString(),
+    })
+
+    expect(result.isRight()).toBe(true)
+    const value = result.value as SuccessResponse<User>
+    expect(value.data?.email).toBe(email)
+    expect(value.data?.name).toBe(name)
+  })
+
+  it('should not be able to get user by wrong id', async () => {
+    const result = await sut.execute({
+      userId: 'invalid-id',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value.data).toBe('Resource not found')
+    expect(result.value.status).toBe(404)
+  })
+})
---
+++ src/application/use-cases/get-user-profile.ts
@@ -0,0 +1,29 @@
+import { FailResponse } from '@/infra/http/entities/fail-response'
+import { SuccessResponse } from '@/infra/http/entities/success-response'
+import { Either, EitherType } from '@cahmoraes93/either'
+import { inject } from '../registry'
+import { UsersRepository } from '../repositories/users-repository'
+import { User } from '../entities/user'
+
+export interface GetUserProfileUseCaseInput {
+  userId: string
+}
+
+export type GetUserProfileUseCaseOutput = EitherType<
+  FailResponse<unknown>,
+  SuccessResponse<User>
+>
+
+export class GetUserProfileUseCase {
+  private usersRepository = inject<UsersRepository>('usersRepository')
+
+  public async execute({
+    userId,
+  }: GetUserProfileUseCaseInput): Promise<GetUserProfileUseCaseOutput> {
+    const userExists = await this.usersRepository.userOfId(userId)
+    if (!userExists) {
+      return Either.left(FailResponse.notFound('Resource not found'))
+    }
+    return Either.right(SuccessResponse.ok(userExists))
+  }
+}
