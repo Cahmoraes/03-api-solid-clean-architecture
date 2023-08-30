8af96c1561a795aea87476d995c78aa242feb153
Caique Moraes <ike.moraes93@gmail.com>
2023-08-30 20:39:26 -0300

    test: CreateGymUseCase

---
+++ src/application/use-cases/create-gym.spec.ts
@@ -0,0 +1,34 @@
+import { provide } from '../registry'
+import { SuccessResponse } from '@/infra/http/entities/success-response'
+import { CreateGymUseCase } from './create-gym'
+import { InMemoryGymsRepository } from 'tests/repositories/in-memory-gyms-repository'
+import { Gym } from '../entities/gym'
+
+describe('CreateGym use case', () => {
+  let gymsRepository: InMemoryGymsRepository
+  let sut: CreateGymUseCase
+
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    provide('gymsRepository', gymsRepository)
+    sut = new CreateGymUseCase()
+  })
+
+  it('should create a gym', async () => {
+    const result = await sut.execute({
+      title: 'Academia TypeScript Gym',
+      latitude: -27.0747279,
+      longitude: -49.4889672,
+      description: 'Fake TypeScript Gym',
+      phone: '00-0000-0000',
+    })
+
+    expect(result.isRight()).toBeTruthy()
+    const value = result.value as SuccessResponse<Gym>
+    expect(value.data?.title).toEqual('Academia TypeScript Gym')
+    expect(value.data?.location.latitude).toEqual(-27.0747279)
+    expect(value.data?.location.longitude).toEqual(-49.4889672)
+    expect(value.data?.description).toEqual('Fake TypeScript Gym')
+    expect(value.data?.phone).toEqual('00-0000-0000')
+  })
+})
