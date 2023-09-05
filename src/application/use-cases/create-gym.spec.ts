52ed8ac08e885d46301a59863fe9abaaa402ba9f
Caique Moraes <ike.moraes93@gmail.com>
2023-09-05 09:19:43 -0300

    test: update create-gym.spec.ts

--- src/application/use-cases/create-gym.spec.ts
+++ src/application/use-cases/create-gym.spec.ts
@@ -3,6 +3,7 @@ import { CreateGymUseCase } from './create-gym.usecase'
 import { InMemoryGymsRepository } from 'tests/repositories/in-memory-gyms-repository'
 import { Gym } from '../entities/gym.entity'
 import { provide } from '@/infra/dependency-inversion/registry'
+import { GymDto } from '../dtos/gym-dto.factory'
 
 describe('CreateGym use case', () => {
   let gymsRepository: InMemoryGymsRepository
@@ -24,10 +25,10 @@ describe('CreateGym use case', () => {
     })
 
     expect(result.isRight()).toBeTruthy()
-    const value = result.value as SuccessResponse<Gym>
+    const value = result.value as SuccessResponse<GymDto>
     expect(value.data?.title).toEqual('Academia TypeScript Gym')
-    expect(value.data?.coord.latitude).toEqual(-27.0747279)
-    expect(value.data?.coord.longitude).toEqual(-49.4889672)
+    expect(value.data.latitude).toEqual(-27.0747279)
+    expect(value.data.longitude).toEqual(-49.4889672)
     expect(value.data?.description).toEqual('Fake TypeScript Gym')
     expect(value.data?.phone).toEqual('00-0000-0000')
   })
