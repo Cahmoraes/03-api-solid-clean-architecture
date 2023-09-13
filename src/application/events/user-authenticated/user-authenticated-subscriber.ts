4289294a0c00d5c9802a4a61a7eef9a06f3adcdd
Caique Moraes <ike.moraes93@gmail.com>
2023-09-13 20:32:06 -0300

    feat: create log

--- src/application/events/user-authenticated/user-authenticated-subscriber.ts
+++ src/application/events/user-authenticated/user-authenticated-subscriber.ts
@@ -1,9 +1,27 @@
 import { DomainEventSubscriber } from '../domain-event-subscriber'
 import { DomainEvents } from '../domain-events.enum'
-import { UserAuthenticatedEvent } from './user-authenticated.event'
+import {
+  UserAuthenticatedEvent,
+  UserAuthenticatedProps,
+} from './user-authenticated.event'
+import { join } from 'path'
+import {
+  ReadStream,
+  WriteStream,
+  createReadStream,
+  createWriteStream,
+} from 'fs'
+import { access, constants, writeFile } from 'fs/promises'
+
+interface UserAuthenticatedLog {
+  email: string
+  id: string
+}
 
 export class UserAuthenticatedSubscriber implements DomainEventSubscriber {
   public readonly eventType: string
+  private readonly FILE_NAME = 'sessions.json'
+  private readonly FILE_PATH = join(process.cwd(), 'log', this.FILE_NAME)
 
   constructor() {
     this.eventType = DomainEvents.USER_AUTHENTICATED
@@ -11,5 +29,73 @@ export class UserAuthenticatedSubscriber implements DomainEventSubscriber {
 
   public handleEvent(aDomainEvent: UserAuthenticatedEvent): void {
     console.log(aDomainEvent.data)
+    this.writeFile(aDomainEvent.data)
+  }
+
+  private async writeFile({
+    id,
+    email,
+  }: UserAuthenticatedProps): Promise<void> {
+    try {
+      await this.createFileIfNotExists()
+      const readStream = this.createReadStream()
+      await this.saveContent(readStream, { id: id.toString(), email })
+    } catch (readError) {
+      console.error('Erro ao ler o arquivo:', readError)
+    }
+  }
+
+  private async createFileIfNotExists() {
+    try {
+      await access(this.FILE_PATH, constants.F_OK)
+    } catch {
+      await writeFile(this.FILE_PATH, '[]')
+    }
+  }
+
+  private createReadStream() {
+    return createReadStream(this.FILE_PATH, 'utf-8')
+  }
+
+  private async saveContent(
+    aReadStream: ReadStream,
+    content: UserAuthenticatedLog,
+  ): Promise<void> {
+    const fileContent = await this.fileContent(aReadStream)
+    aReadStream.on('end', async () => {
+      try {
+        fileContent.push(content)
+        const writeStream = this.createWriteStream()
+        this.write(writeStream, fileContent)
+      } catch (parseError) {
+        console.error('Erro ao analisar o conteúdo do arquivo:', parseError)
+      }
+    })
+  }
+
+  private fileContent(
+    aReadStream: ReadStream,
+  ): Promise<UserAuthenticatedLog[]> {
+    return new Promise((resolve) => {
+      aReadStream.on('data', (chunk) => {
+        try {
+          resolve(JSON.parse(chunk.toString()))
+        } catch {
+          resolve([])
+        }
+      })
+    })
+  }
+
+  private createWriteStream(): WriteStream {
+    const writeStream = createWriteStream(this.FILE_PATH, 'utf-8')
+    writeStream.on('finish', () => {
+      console.log('Arquivo atualizado com sucesso.')
+    })
+    return writeStream
+  }
+
+  private write(aWriteStream: WriteStream, content: unknown[]) {
+    aWriteStream.write(JSON.stringify(content))
   }
 }
--- src/application/events/user-authenticated/user-authenticated.event.ts
+++ src/application/events/user-authenticated/user-authenticated.event.ts
@@ -2,16 +2,16 @@ import { UniqueIdentity } from '@/core/entities/value-objects/unique-identity'
 import { DomainEvent } from '../domain-event'
 import { DomainEvents } from '../domain-events.enum'
 
-export interface UserCreatedProps {
+export interface UserAuthenticatedProps {
   id: UniqueIdentity
   email: string
 }
 
 export class UserAuthenticatedEvent implements DomainEvent {
   public readonly eventType: string
-  public readonly data: UserCreatedProps
+  public readonly data: UserAuthenticatedProps
 
-  constructor(props: UserCreatedProps) {
+  constructor(props: UserAuthenticatedProps) {
     this.eventType = DomainEvents.USER_AUTHENTICATED
     this.data = props
   }
