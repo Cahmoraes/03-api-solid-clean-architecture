d3327d86d9b1258382613d941152e12c83ac3d55
Caique Moraes <ike.moraes93@gmail.com>
2023-09-16 13:14:42 -0300

    feat: update-password.usecase

---
+++ src/application/use-cases/update-password.usecase.ts
@@ -0,0 +1,42 @@
+import { Either, EitherType } from '@cahmoraes93/either'
+import { UserDto, UserDtoFactory } from '../dtos/user-dto.factory'
+import { UsersRepository } from '../repositories/users-repository'
+import { inject } from '@/infra/dependency-inversion/registry'
+import { ResourceNotFoundError } from '../errors/resource-not-found.error'
+import { Password } from '../entities/value-objects/password'
+import { PasswordValidatorError } from '../entities/errors/password-validator.error'
+
+interface UpdatePasswordUseCaseInput {
+  userId: string
+  password: string
+}
+
+type UpdatePasswordUseCaseOutput = EitherType<
+  Error | ResourceNotFoundError | PasswordValidatorError,
+  UserDto
+>
+
+export class UpdatePasswordUseCase {
+  private usersRepository = inject<UsersRepository>('usersRepository')
+
+  async execute({
+    userId,
+    password,
+  }: UpdatePasswordUseCaseInput): Promise<UpdatePasswordUseCaseOutput> {
+    const user = await this.usersRepository.userOfId(userId)
+    if (!user) return Either.left(new ResourceNotFoundError())
+    const newPasswordOrError = await this.createPassword(password)
+    if (newPasswordOrError.isLeft()) {
+      return Either.left(newPasswordOrError.value)
+    }
+    user.updatePassword(newPasswordOrError.value)
+    await this.usersRepository.save(user)
+    return Either.right(UserDtoFactory.create(user))
+  }
+
+  private async createPassword(
+    aPassword: string,
+  ): Promise<EitherType<PasswordValidatorError, Password>> {
+    return Password.create(aPassword)
+  }
+}
