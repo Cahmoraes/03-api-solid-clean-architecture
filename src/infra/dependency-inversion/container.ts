469a2efa29b0ae1370d97d4b58fbf56df93c9afb
Caique Moraes <ike.moraes93@gmail.com>
2023-09-18 18:59:34 -0300

    refactor: extract module

---
+++ src/infra/dependency-inversion/container.ts
@@ -0,0 +1,33 @@
+import { AuthenticateUseCase } from '@/application/use-cases/authenticate.usecase'
+import { CreateCheckInUseCase } from '@/application/use-cases/create-check-in.usecase'
+import { CreateGymUseCase } from '@/application/use-cases/create-gym.usecase'
+import { CreateUserUseCase } from '@/application/use-cases/create-user.usecase'
+import { FetchNearbyGymsUseCase } from '@/application/use-cases/fetch-nearby-gym.usecase'
+import { FetchUserCheckInsHistoryUseCase } from '@/application/use-cases/fetch-user-check-ins-history.usecase'
+import { GetUserMetricsUseCase } from '@/application/use-cases/get-user-metrics.usecase'
+import { GetUserProfileUseCase } from '@/application/use-cases/get-user-profile.usecase'
+import { SearchGymsUseCase } from '@/application/use-cases/search-gyms.usecase'
+import { UpdatePasswordUseCase } from '@/application/use-cases/update-password.usecase'
+import { ValidateCheckInUseCase } from '@/application/use-cases/validate-check-in.usecase'
+import { PrismaCheckInsRepository } from '../repositories/prisma/prisma-check-ins-repository'
+import { PrismaGymsRepository } from '../repositories/prisma/prisma-gyms-repository'
+import { PrismaUsersRepository } from '../repositories/prisma/prisma-users-repository'
+import { provide } from './registry'
+
+provide('usersRepository', new PrismaUsersRepository())
+provide('gymsRepository', new PrismaGymsRepository())
+provide('checkInsRepository', new PrismaCheckInsRepository())
+provide('createUserUseCase', new CreateUserUseCase())
+provide('authenticateUseCase', new AuthenticateUseCase())
+provide('getUserMetricsUseCase', new GetUserMetricsUseCase())
+provide('getUserProfileUseCase', new GetUserProfileUseCase())
+provide('createGymUseCase', new CreateGymUseCase())
+provide('searchGymsUseCase', new SearchGymsUseCase())
+provide('fetchNearbyGymsUseCase', new FetchNearbyGymsUseCase())
+provide('createCheckInUseCase', new CreateCheckInUseCase())
+provide('validateCheckInUseCase', new ValidateCheckInUseCase())
+provide(
+  'fetchUserCheckInsHistoryUseCase',
+  new FetchUserCheckInsHistoryUseCase(),
+)
+provide('updatePasswordUseCase', new UpdatePasswordUseCase())
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
-import { CreateUserUseCase } from './application/use-cases/create-user.usecase'
-import { FastifyAdapter } from './infra/http/servers/fastify/fastify-adapter'
-import { AuthenticateUseCase } from './application/use-cases/authenticate.usecase'
-import { provide } from './infra/dependency-inversion/registry'
-import { PrismaUsersRepository } from './infra/repositories/prisma/prisma-users-repository'
-import { GetUserMetricsUseCase } from './application/use-cases/get-user-metrics.usecase'
+import '@/infra/dependency-inversion/container'
 import { DomainEventPublisher } from './application/events/domain-event-publisher'
 import { UserCreatedSubscriber } from './application/events/user-created/user-created-subscriber'
-import { GetUserProfileUseCase } from './application/use-cases/get-user-profile.usecase'
-import { CreateGymUseCase } from './application/use-cases/create-gym.usecase'
-import { PrismaGymsRepository } from './infra/repositories/prisma/prisma-gyms-repository'
-import { SearchGymsUseCase } from './application/use-cases/search-gyms.usecase'
-import { FetchNearbyGymsUseCase } from './application/use-cases/fetch-nearby-gym.usecase'
-import { CreateCheckInUseCase } from './application/use-cases/create-check-in.usecase'
-import { ValidateCheckInUseCase } from './application/use-cases/validate-check-in.usecase'
-import { PrismaCheckInsRepository } from './infra/repositories/prisma/prisma-check-ins-repository'
-import { FetchUserCheckInsHistoryUseCase } from './application/use-cases/fetch-user-check-ins-history.usecase'
 import { GymCreatedSubscriber } from './application/events/gym-created/gym-created-subscriber'
 import { UserAuthenticatedSubscriber } from './application/events/user-authenticated/user-authenticated-subscriber'
 import { ProductionLogger } from './infra/logger/production-logger'
-import { UpdatePasswordUseCase } from './application/use-cases/update-password.usecase'
 import { HttpControllerFactory } from './infra/http/controllers/factories/http-controller-factory'
 import { HttpServerFactory } from './infra/http/servers/factories/http-server-factory'
 import { serverArgv } from './argv'
@@ -28,23 +13,7 @@ DomainEventPublisher.getInstance().subscribe(new GymCreatedSubscriber())
 DomainEventPublisher.getInstance().subscribe(
   new UserAuthenticatedSubscriber(new ProductionLogger()),
 )
-provide('usersRepository', new PrismaUsersRepository())
-provide('gymsRepository', new PrismaGymsRepository())
-provide('checkInsRepository', new PrismaCheckInsRepository())
-provide('createUserUseCase', new CreateUserUseCase())
-provide('authenticateUseCase', new AuthenticateUseCase())
-provide('getUserMetricsUseCase', new GetUserMetricsUseCase())
-provide('getUserProfileUseCase', new GetUserProfileUseCase())
-provide('createGymUseCase', new CreateGymUseCase())
-provide('searchGymsUseCase', new SearchGymsUseCase())
-provide('fetchNearbyGymsUseCase', new FetchNearbyGymsUseCase())
-provide('createCheckInUseCase', new CreateCheckInUseCase())
-provide('validateCheckInUseCase', new ValidateCheckInUseCase())
-provide(
-  'fetchUserCheckInsHistoryUseCase',
-  new FetchUserCheckInsHistoryUseCase(),
-)
-provide('updatePasswordUseCase', new UpdatePasswordUseCase())
+
 const httpServer = HttpServerFactory.create(serverArgv)
 const HttpController = HttpControllerFactory.create(httpServer)
 new HttpController(httpServer)
