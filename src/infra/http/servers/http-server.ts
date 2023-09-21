a2d960c29a072c07ca81261f544da6fe6197ee9a
Caique Moraes <ike.moraes93@gmail.com>
2023-09-21 13:59:18 -0300

    refactor: update type HttpServer

--- src/infra/http/servers/http-server.ts
+++ src/infra/http/servers/http-server.ts
@@ -37,7 +37,7 @@ export type HttpHandler = (
   httpHandlerParams: HttpHandlerParams,
 ) => Promise<EitherType<FailResponse<unknown>, SuccessResponse<unknown>>>
 
-type Middleware = (...args: object[]) => void
+type Middleware = (...args: any[]) => void
 
 export interface MiddlewareProps {
   onRequest: Middleware | Middleware[]
